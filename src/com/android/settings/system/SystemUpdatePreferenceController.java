a8a47de64f86f10e78e858c32dab1a87205478bc
iamtoxyc <vtechnical1804@gmail.com>
2020-06-03 21:36:20 +0530

    Fix Ota

--- src/com/android/settings/system/SystemUpdatePreferenceController.java
+++ src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -35,7 +35,7 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
     private static final String KEY_SYSTEM_UPDATE_SETTINGS = "system_update_settings";
 
     private static final String OTA_BUILD_TYPE_PROP = "ro.wrath.build_type";
-    private static final String OTA_APP_PACKAGE = "ro.wrath.ota";
+    private static final String OTA_APP_PACKAGE = "com.wrath.ota";
 
     private final UserManager mUm;
 
