Havoc-OS/android_packages_apps_Settings

Settings: Bring back Gaming mode [2/3]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -151,6 +151,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.android.settings.GesturesSettings;
 
 import com.havoc.config.center.ConfigCenter;
+import com.havoc.config.center.fragments.GamingMode;
 
 public class SettingsGateway {
 
@@ -290,7 +291,8 @@ public class SettingsGateway {
             GlobalActionsPanelSettings.class.getName(),
             LiveDisplaySettings.class.getName(),
             ConfigCenter.class.getName(),
-            GesturesSettings.class.getName()
+            GesturesSettings.class.getName(),
+            GamingMode.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -330,5 +332,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.ConfigCenterActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
     };
 }

Dixzz/android_packages_apps_Settings-2

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -149,7 +149,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.wrath.settings.WrathSettings;
-import com.wrath.settings.WrathSettings;
+import com.wrath.settings.fragments.GamingMode;
 import com.android.settings.livedisplay.LiveDisplaySettings;
 
 public class SettingsGateway {
@@ -288,8 +288,9 @@ public class SettingsGateway {
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
             DarkModeSettingsFragment.class.getName(),
-            WrathSettings.class.getName()
-            LiveDisplaySettings.class.getName()
+	        WrathSettings.class.getName(),
+            LiveDisplaySettings.class.getName(),
+            GamingMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -328,7 +329,9 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+
 	    //Wrath
 	    Settings.WrathSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
     };
 }